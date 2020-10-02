812a359aac1d7cb6c3001dcdd7001ced13871e5e
TUTORIAL-termux-github <71549156+TUTORIAL-termux-github@users.noreply.github.com>
2020-10-02 22:16:09 +0700

    Create Scan.py

---
+++ Scan.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import os,sys,socket
+os.system('clear')
+print "[•] Menyiapkan Installasi"
+os.system('clear')
+os.system('pkg install nmap -y')
+os.system('clear')
+os.system('pkg install python')
+os.system('pkg install toilet')
+os.system('clear')
+print "[√] Installasi Selesai"
+os.system('clear')
+pilih = raw_input('pilih nomor:'nmap -sn)
+if pilih == "1":
+ 
+
+              
