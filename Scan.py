fbab90ca88b1fd84821c785ab8767f27189830db
TUTORIAL-termux-github <71549156+TUTORIAL-termux-github@users.noreply.github.com>
2020-10-03 12:37:41 +0700

    Update Scan.py

--- Scan.py
+++ Scan.py
@@ -4,14 +4,15 @@ os.system('clear')
 print "[•] Menyiapkan Installasi"
 os.system('clear')
 os.system('pkg install nmap -y')
+os.system('pkg update && pkg upgrade')
 os.system('clear')
 os.system('pkg install python')
 os.system('pkg install toilet')
 os.system('clear')
 print "[√] Installasi Selesai"
 os.system('clear')
-pilih = raw_input('pilih nomor:'nmap -sn)
+pilih = raw_input('pilih nomor:')
 if pilih == "1":
- 
+   os.system('pkg update')
 
               
