4a5d2e4d9b308d51fbaf0267ece599226ff9f1c3
TUTORIAL-termux-github <71549156+TUTORIAL-termux-github@users.noreply.github.com>
2020-10-21 15:41:03 +0700

    Update Scan.py

--- Scan.py
+++ Scan.py
@@ -1,21 +1,30 @@
-#!/usr/bin/python2
-# -*- coding: utf-8 -*-
-import os,sys,socket
-os.system('clear')
-print "[•] Menyiapkan Installasi"
-os.system('clear')
-os.system('pkg install nmap -y')
-os.system('pkg update && pkg upgrade')
-os.system('clear')
-os.system('pkg install python')
-os.system('pkg install toilet')
-os.system('clear')
-print "[√] Installasi Selesai"
-os.system('clear')
-pilih = raw_input('pilih nomor:')
-if pilih == "1":
-print "ketik (0) untuk keluar"
-print "[√] Scan Complete"
-elif pilih == "0":
-exit()
-              
+#!/usr/bin/python
+# coding: utf-8
+
+import os
+import sys
+import socket
+os.system("clear")
+os.system("echo TOOL INI BERGUNA UNTUK MENCARI HALAMAN WEB |lolcat")
+os.system("echo TARGET YANG TERDAPAT CELAH SQL dan XSS  |lolcat")
+os.system("sleep 5")
+os.system("clear")
+os.system("figlet SQL-SEARCH")
+print "V 0.1"
+print ""
+print "======================================="
+os.system("echo Author:R3D BULL BAR² |lolcat")
+os.system("echo Youtube:R3D BULL CYBER |lolcat")
+os.system("echo github:https://github.com/TUTORIAL-termux-github |lolcat")
+print "======================================="
+print ""
+os.system("echo [!] Awal url tidak boleh menggunakan = https/https |lolcat")
+name=raw_input('Masukkan web: ')
+os.system("clear")
+print("sedang mencari celah keamanan sql injection | %s |" % name)
+print "Mohon tunggu beberapa saat"
+addr=socket.gethostbyname(name)
+print("ip webnya {} = {}".format(name, addr))
+os.system("google --rua %s php id=" % name)
+os.system("google --rua %s php?cid=" % name)
+print "Scan web selesai"
